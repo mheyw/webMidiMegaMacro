e230cb6c2d75d11ee9d8f5df14b6b4d16b8b5360
mheyw <50662869+mheyw@users.noreply.github.com>
2021-07-27 12:21:08 +0100

    Update Files

--- libs/learner.v.0.3.js
+++ libs/learner.v.0.3.js
@@ -5,12 +5,11 @@ class Learner {
  /**
   * Creates the main Learner Object
   * @param {Object} options - options for initialisation
-    @param {string} options.docId - Id used for storing data, defaults to
-    frameElement name
+    @param {string} options.modelName - Id used for storing data. You only need to provide this if you have two instances of Learner.js running, this name is used to distinguish the datasets.
     @param {function} options.onLoad - Called after all the libraries have set up
   * @example
   * new Learner({
-  *   docId:"myDoc"
+  *   modelName:"classifier1"
   *   onLoad:()=>{
   *     console.log("I finished loading!")
   *   }
@@ -18,23 +17,26 @@ class Learner {
 
   */
   constructor(options) {
-    let docId;
+    let modelName = "";
+    let docIdPrefix;
+    if(window.frameElement) {
+      docIdPrefix = window.frameElement.name;
+    }
+    let databaseName = "";
+    if(docIdPrefix === undefined)
+    {
+      docIdPrefix = "local";
+    }
     if(options !== undefined)
     {
-      docId = options.docId
+      modelName = options.modelName
     }
-    if(docId === undefined)
-    {
-      docId = window.frameElement.name;
-    }
-    if(docId === undefined)
-    {
-      docId = "local";
-    }
+    databaseName = docIdPrefix + "_" + modelName;
     this.USE_WORKER = true;
     this.outputGUI = [];
     this.modelOptions = {};
-    this.classifier = true;
+    //Classifier 0, Regression 1, Series 2
+    this.modelType = 0;
     this.recordingRound = 0;
     /** Is currently recording
         @var {boolean} */
@@ -57,7 +59,7 @@ class Learner {
     storageScript.onload = ()=>{
       this.DATASET_KEY = "dataset";
       this.REC_KEY = "recordingRound";
-      this.store = localforage.createInstance({name: docId});
+      this.store = localforage.createInstance({name: databaseName});
       console.log("loaded localforage", this.store);
       this.store.getItem(this.DATASET_KEY).then((dataset)=> {
         if(!dataset)
@@ -73,7 +75,7 @@ class Learner {
           });
           console.log("dataset exists of size " + dataset.length);
         }
-        this.updateRows();
+        this.updateNumExamples();
       });
       if(options.onLoad !== undefined) {
         options.onLoad();
@@ -126,6 +128,15 @@ class Learner {
       *   sound.volume = data[1]
       *   sound.lfo = data[2]
       * }
+      * @example
+      * //Series Classification
+      * this.onOutput = (data)=> {
+      *   if(data[0] == 0) {
+      *     sound.trigger()
+      *   } else {
+      *     sound2.trigger()
+      *   }
+      * }
         @var {function}
      */
     this.onOutput = ()=> {
@@ -153,86 +164,81 @@ class Learner {
     let row = table.insertRow();
     let cell = row.insertCell();
     cell.colSpan = 2;
-    let recBtn = document.createElement("BUTTON");
-    recBtn.classList.add("learner-btn")
-    recBtn.id = "rec-btn";
-    recBtn.onclick = ()=>{
-      this.record();
+    this.recBtn = document.createElement("BUTTON");
+    this.recBtn.classList.add("learner-btn")
+    this.recBtn.onclick = ()=>{
+      this.toggleRecord();
     };
-    recBtn.innerHTML = "Record";
-    cell.appendChild(recBtn);
+    this.recBtn.innerHTML = "Record";
+    cell.appendChild(this.recBtn);
     cell = row.insertCell();
     cell.colSpan = 2;
-    const countDown = document.createElement('span')
-    countDown.id = "countdown-span";
-    countDown.classList.add("learner-label");
-    cell.appendChild(countDown);
+    this.countDown = document.createElement('span')
+    this.countDown.classList.add("learner-label");
+    cell.appendChild(this.countDown);
 
 	  row = table.insertRow();
     cell = row.insertCell();
     cell.colSpan = 2;
-    let trainBtn = document.createElement("BUTTON");
-    trainBtn.id = "train-btn";
-    trainBtn.classList.add("learner-btn")
-    trainBtn.onclick = ()=>{
+    this.trainBtn = document.createElement("BUTTON");
+    this.trainBtn.classList.add("learner-btn")
+    this.trainBtn.onclick = ()=>{
       this.train();
     };
-    trainBtn.innerHTML = "Train";
-    cell.appendChild(trainBtn);
+    this.trainBtn.innerHTML = "Train";
+    cell.appendChild(this.trainBtn);
 
     row = table.insertRow();
     cell = row.insertCell();
     cell.colSpan = 2;
-    let runBtn = document.createElement("BUTTON");
-    runBtn.id = "run-btn";
-    runBtn.onclick = ()=>{
-      this.run();
+    this.runBtn = document.createElement("BUTTON");
+    this.runBtn.onclick = ()=>{
+      this.toggleRun();
     };
-    runBtn.innerHTML = "Run";
-    runBtn.classList.add("learner-btn")
-    runBtn.disabled = true;
-    cell.appendChild(runBtn);
+    this.runBtn.innerHTML = "Run";
+    this.runBtn.classList.add("learner-btn")
+    this.runBtn.disabled = true;
+    cell.appendChild(this.runBtn);
 
     row = table.insertRow();
     cell = row.insertCell();
     cell.colSpan = 1;
-    let deleteLastBtn = document.createElement("BUTTON");
-    deleteLastBtn.classList.add("learner-btn")
-    deleteLastBtn.classList.add("learner-btn-3");
-    deleteLastBtn.onclick = ()=>{
+    this.deleteLastBtn = document.createElement("BUTTON");
+    this.deleteLastBtn.classList.add("learner-btn")
+    this.deleteLastBtn.classList.add("learner-btn-3");
+    this.deleteLastBtn.onclick = ()=>{
       this.deleteLastRound();
     };
-    deleteLastBtn.innerHTML = "Clear Prev";
-    cell.appendChild(deleteLastBtn);
+    this.deleteLastBtn.innerHTML = "Clear Prev";
+    cell.appendChild(this.deleteLastBtn);
 
-    let deleteBtn = document.createElement("BUTTON");
+    this.deleteBtn = document.createElement("BUTTON");
     cell = row.insertCell();
     cell.colSpan = 1;
-    deleteBtn.classList.add("learner-btn");
-    deleteBtn.classList.add("learner-btn-3");
-    deleteBtn.onclick = ()=>{
+    this.deleteBtn.classList.add("learner-btn");
+    this.deleteBtn.classList.add("learner-btn-3");
+    this.deleteBtn.onclick = ()=>{
       this.clear();
     };
-    deleteBtn.innerHTML = "Clear All";
-    cell.appendChild(deleteBtn);
+    this.deleteBtn.innerHTML = "Clear All";
+    cell.appendChild(this.deleteBtn);
 
     cell = row.insertCell();
     cell.colSpan = 2;
-    let saveBtn = document.createElement("BUTTON");
-    saveBtn.classList.add("learner-btn");
-    saveBtn.onclick = ()=>{
+    this.saveBtn = document.createElement("BUTTON");
+    this.saveBtn.classList.add("learner-btn");
+    this.saveBtn.onclick = ()=>{
       this.downloadTrainingData();
     };
-    saveBtn.innerHTML = "Download Data";
-    cell.appendChild(saveBtn);
+    this.saveBtn.innerHTML = "Download Data";
+    cell.appendChild(this.saveBtn);
 
     row = table.insertRow();
     cell = row.insertCell();
     cell.colSpan = 2;
-    let datalog = document.createElement('span')
-    datalog.id = "datalog";
-    datalog.classList.add("learner-label");
-    cell.appendChild(datalog);
+    this.datalog = document.createElement('span')
+    this.datalog.classList.add("learner-label");
+    cell.appendChild(this.datalog);
 
     this.outputLabel = document.createElement("span");
     this.outputLabel.innerHTML = "Select your outputs"
@@ -250,7 +256,7 @@ class Learner {
 
     this.guiParent = parent;
 
-    this.updateRows();
+    this.updateNumExamples();
   }
 
   newRegression() {
@@ -289,7 +295,7 @@ class Learner {
         this.newRegression();
       }
 
-      this.classifier = false;
+      this.modelType = 1;
       this.numOutputs = n;
       this.gui = gui;
       for(let i = 0; i < n; i++)
@@ -344,9 +350,40 @@ class Learner {
     {
       this.newClassifier();
     }
-    this.classifier = true;
+    this.modelType = 0;
+    this.addDropdownGui(n, gui, smoothOutput)
+  }
+
+  /**
+    Add a series model
+    @param {number} outputs - the number of gestures for the series model
+    @param {boolean} [gui = true]  - Include a gui
+    @param {number} [numFrames = 0]  - How many frames to smooth the output over
+   */
+  addSeries(
+      n,
+      gui = true,
+      smoothOutput = 0)
+  {
+    let origin = document.location.origin
+    if(origin.includes("file"))
+    {
+      origin = "http://127.0.0.1:4200"
+    }
+    const workerUrl = origin + "/libs/seriesWorker.js"
+    if(this.USE_WORKER)
+    {
+      this.setWorker(workerUrl)
+    }
+    this.modelType = 2;
+
+    this.addDropdownGui(n, gui, smoothOutput)
+  }
+
+  addDropdownGui(n, gui, smoothOutput) {
     this.numOutputs = 1;
     this.gui = gui;
+    this.numClasses = n
     this.y.push(0);
     if(gui)
     {
@@ -396,32 +433,33 @@ class Learner {
 
   disableButtons(disable)
   {
-    const run = document.getElementById("run-btn");
-    const rec = document.getElementById("rec-btn");
-    const train = document.getElementById("train-btn");
-    run.disabled = rec.disabled = train.disabled = disable;
+    this.runBtn.disabled = this.recBtn.disabled = this.trainBtn.disabled = disable;
   }
 
   updateButtons() {
-    const run = document.getElementById("run-btn");
-    const rec = document.getElementById("rec-btn");
-    const train = document.getElementById("train-btn");
-    run.innerHTML = this.running ? "Stop" : "Run"
-    rec.innerHTML = this.recording ? "Stop" : "Record"
-    rec.disabled = this.running;
-    run.disabled = this.recording;
-    train.disabled = this.recording || this.running;
+    this.runBtn.innerHTML = this.running ? "Stop" : "Run"
+    this.recBtn.innerHTML = this.recording ? "Stop" : "Record"
+    this.recBtn.disabled = this.running;
+    this.runBtn.disabled = this.recording;
+    this.trainBtn.disabled = this.recording || this.running;
     if(this.onUpdateState)
     {
       this.onUpdateState();
     }
   }
 
-  updateRows() {
-    const datalog = document.getElementById("datalog");
+  updateNumExamples() {
     this.numRows().then((n)=>{
-      const total = n + this.temp.length;
-      datalog.innerHTML = "You have " + total + " saved examples";
+      let total = 0;
+      if(Array.isArray(n))
+      {
+        total = n;
+      }
+      else
+      {
+         total = n + this.temp.length;
+      }
+      this.datalog.innerHTML = "You have " + total + " saved examples";
     });
   }
 
@@ -455,7 +493,7 @@ class Learner {
   @example
   learner.newExample([1,2,3,4],[5,6])
   */
-  newExample(input, y) {
+  newExample(input, y = []) {
     //Convert to Array if TypedArray
     if(this.isTypedArray(input))
     {
@@ -485,7 +523,14 @@ class Learner {
       //RUN
       if(this.USE_WORKER)
       {
-        this.myWorker.postMessage({action:"run",data:input});
+        if(this.modelType == 2)
+        {
+          this.temp.push(JSON.parse(JSON.stringify(input)));
+        }
+        else
+        {
+          this.myWorker.postMessage({action:"run",data:input});
+        }
       }
       else
       {
@@ -548,6 +593,7 @@ class Learner {
   setWorker(url) {
     this.myWorker = this.createWorker(url);
     this.myWorker.onmessage = (event)=>{
+      console.log(event)
       if(event.data == "trainingend")
       {
         this.trainingEnd()
@@ -567,7 +613,7 @@ class Learner {
     {
       this.disableButtons(true);
       this.trainingData().then((t)=> {
-        this.updateRows();
+        this.updateNumExamples();
         if(this.USE_WORKER)
         {
           this.myWorker.postMessage({action:"train",data:t});
@@ -583,14 +629,23 @@ class Learner {
 
   trainingEnd() {
     this.disableButtons(false);
-    this.run()
+    if(this.modelType !== 2)
+    {
+      this.toggleRun()
+    }
   }
+
   /**
-  Run the current model
+  Run/Stop the current model
    */
-  run() {
+  toggleRun() {
     this.recording = false;
     this.running = !this.running;
+    if(!this.running && this.modelType == 2)
+    {
+      this.myWorker.postMessage({action:"run",data:this.temp});
+      this.temp = [];
+    }
     this.updateButtons();
   }
 
@@ -604,7 +659,7 @@ class Learner {
         {
           this.streamBuffers[i].push(output)
           output = this.streamBuffers[i].mean();
-          if(this.classifier)
+          if(this.modelType == 0)
           {
             output = Math.round(output);
           }
@@ -621,25 +676,28 @@ class Learner {
 
   limitRecord() {
     let timeLeft = this.recLimit;
-    const label = document.getElementById("countdown-span");
+    const label = this.cound
     this.stopInterval = setInterval(()=>{
       timeLeft -= 1;
       label.innerHTML = "stopping in " + timeLeft + " secs"
     }, 1000);
     this.stopTimeout = setTimeout(()=>{
       this.stopTimeout = null;
-      this.record();
+      this.toggleRecord();
     }, this.countIn * 1000)
   }
 
-  record() {
+ /**
+  Start/Stop recording
+  */
+  toggleRecord() {
     if(this.stopInterval)
     {
       clearTimeout(this.stopTimeout);
       clearInterval(this.stopInterval);
       this.stopInterval = null;
       this.stopTimeout = null;
-      const label = document.getElementById("countdown-span");
+      const label = this.countDown;
       label.innerHTML = "";
     }
     const doRun = ()=> {
@@ -647,22 +705,24 @@ class Learner {
       this.recording = !this.recording;
       if(!this.recording)
       {
-        this.newRecordingRound();
         this.save();
       }
-      else if(this.recLimit > 0)
+      else
       {
-		    this.limitRecord();
+        this.newRecordingRound();
+        if(this.recLimit > 0)
+        {
+		      this.limitRecord();
+        }
       }
       this.updateButtons();
-      const run = document.getElementById("run-btn");
-      run.disabled = true;
+      this.runBtn.disabled = true;
     }
     if(this.countIn > 0 && !this.recording)
     {
       let timeLeft = this.countIn;
-      const label = document.getElementById("countdown-span");
-      const rec = document.getElementById("rec-btn");
+      const label = this.countDown
+      const rec = this.recBtn;
       rec.disabled = true;
       let interval = setInterval(()=>{
         timeLeft -= 1;
@@ -686,7 +746,7 @@ class Learner {
   clear() {
     return new Promise((resolve, reject)=> {
       this.store.setItem(this.DATASET_KEY,[]).then(()=> {
-          this.updateRows();
+          this.updateNumExamples();
           resolve();
       });
     })
@@ -716,35 +776,57 @@ class Learner {
    */
   deleteLastRound() {
     this.store.getItem(this.DATASET_KEY).then((dataset)=> {
-        let trainingData = [];
+      let trainingData = [];
+      if(this.modelType == 2)
+      {
+        dataset.pop();
+        trainingData = dataset;
+      }
+      else
+      {
         dataset.forEach((line)=> {
           if(line.recordingRound < this.recordingRound - 1)
           {
             trainingData.push({input:line.input, output:line.output});
           }
         });
-      	this.recordingRound--;
-      	this.store.setItem(this.REC_KEY, this.recordingRound);
-        this.store.setItem(this.DATASET_KEY, trainingData).then(()=> {
-          this.updateRows();
-        });
+        this.recordingRound--;
+        this.store.setItem(this.REC_KEY, this.recordingRound);
+      }
+      this.store.setItem(this.DATASET_KEY, trainingData).then(()=> {
+        this.updateNumExamples();
       });
+    });
   }
 
   addRow(newInputs, newOutputs) {
-    this.temp.push({input:JSON.parse(JSON.stringify(newInputs)),
-                    output:JSON.parse(JSON.stringify(newOutputs)),
-                    recordingRound:this.recordingRound});
-    this.updateRows();
+    if(this.modelType !== 2)
+    {
+      this.temp.push({input:JSON.parse(JSON.stringify(newInputs)),
+                      output:JSON.parse(JSON.stringify(newOutputs)),
+                      recordingRound:this.recordingRound});
+    }
+    else
+    {
+      this.temp.push(JSON.parse(JSON.stringify(newInputs)));
+    }
+    this.updateNumExamples();
   }
 
   save() {
     return new Promise((resolve, reject)=> {
       this.store.getItem(this.DATASET_KEY).then((dataset)=> {
-        dataset = dataset.concat(this.temp);
+        if(this.modelType == 2 && this.temp.length > 0)
+        {
+          dataset.push({input:this.temp, label:""+this.y[0]})
+        }
+        else
+        {
+          dataset = dataset.concat(this.temp);
+        }
         this.temp = [];
         this.store.setItem(this.DATASET_KEY, dataset).then(()=> {
-          this.updateRows();
+          this.updateNumExamples();
           resolve();
         });
       });
@@ -765,9 +847,20 @@ class Learner {
       else
       {
         this.store.getItem(this.DATASET_KEY).then((dataset)=> {
-          if(dataset)
+          if(dataset !== undefined)
           {
-            resolve(dataset.length);
+            if(this.modelType === 2) {
+              let vals = Array(this.numClasses).fill(0);
+              dataset.forEach((s)=> {
+                vals[parseInt(s.label)]++;
+              });
+              resolve(vals);
+            }
+            else
+            {
+              resolve(dataset.length);
+            }
+
           }
           else
           {
@@ -786,12 +879,7 @@ class Learner {
     return new Promise((resolve, reject)=> {
       this.save().then(()=> {
         this.store.getItem(this.DATASET_KEY).then((dataset)=> {
-          let trainingData = [];
-          dataset.forEach((line)=> {
-            let l = {input:line.input, output:line.output};
-            trainingData.push(l);
-          });
-          resolve(trainingData);
+          resolve(dataset);
         });
       });
     });
